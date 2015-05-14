168c5ace944e6059d6f153b7a8417d31295c3a3a
jamie_dixon <jamie@tenfingersfree.com>
2015-05-14 12:55:34 -0400

    Added more examples

---
+++ Source/Script1.R
@@ -0,0 +1,28 @@
+
+numbers <- c(1.0,2.0,3.1,1.0,2.0,3.1)
+is.factor(numbers)
+numbers <- factor(numbers)
+is.factor(numbers)
+numbers
+
+ints <- c(1,2,3,1,2,3)
+is.factor(ints)
+ints <- factor(ints)
+is.factor(ints)
+ints
+
+characters <- c("Larry","Curlie","Moe","Larry")
+is.factor(characters)
+characters <- factor(characters)
+is.factor(characters)
+characters
+
+logicals <- c(TRUE,TRUE,FALSE)
+is.factor(logicals)
+logicals <- factor(logicals)
+is.factor(logicals)
+logicals
+
+
+
+
