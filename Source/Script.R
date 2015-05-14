17f5536f182dc4ba6ac67336e9184297e33b156e
Jamie Dixon <jamessdixon@gmail.com>
2015-05-14 12:50:44 -0400

    Initial commit

---
+++ Source/Script.R
@@ -0,0 +1,23 @@
+
+x <- 10
+x
+summary(x)
+length(x)
+
+x <- 10
+#y <- null
+y <- NA
+z <- x + y
+is.na(z)
+
+
+v1 <- c(3,4,5)
+mean(v1)
+sqrt(v1)
+
+v2 <- c(6,7,8)
+v3 <- v1+v2
+
+#str = structure (not string)
+str(v3) 
+
