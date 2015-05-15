1226c4eae734f207205a7b2ea543480e1a9a91a0
Jamie Dixon <jamessdixon@gmail.com>
2015-05-15 10:03:00 -0400

    Added import amd k-means to flowers analysis

--- Source/Experiment2.R
+++ Source/Experiment2.R
@@ -1,6 +1,19 @@
 #http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data
 
+#flowers <- read.csv("http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data",blank.lines.skip=FALSE)
 flowers <- read.csv("http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
+summary(flowers)
+
+colnames(flowers) <- c("F1", "F2", "F3", "F4", "Label")
+summary(flowers)
+
+
+indexes = sample(1:nrow(flowers), size=0.6*nrow(flowers))
+flowers.train <- flowers[-indexes,]
+flowers.test <- flowers[indexes,]
+
+#fit <- kmeans(flowers.train, 5)
+
 
 
 
